9c7f49bf32959c5156219491903eb374bc2d511f
Pemberai Sweto <pembeweb@yahoo.com>
2020-03-31 00:01:38 +0200

    feat: setup the web server

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {}
+
+var bodyParser = require('body-parser')
 
 // Require Express to run server and routes
-
+const express = require('express')
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(bodyParser.json())
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static('website'))
 
 // Setup Server
+const port = 3000
+const server = app.listen(port, listening)
+
+function listening () {
+  console.log('server runing')
+  console.log(`running on local host : ${port}`)
+}
+
+// GET Method
+app.get('/getRecentData', (req, res) => {
+  res.send(JSON.stringify(projectData))
+})
+
+// POST Method
+app.post('/saveData', saveData)
+
+function saveData (req, res) {
+  let responseData = req.body
+  if (responseData.temperature) {
+    let newEntry = {
+      temperature: responseData.temperature,
+      date: responseData.date,
+      content: responseData.content
+    }
+    projectData = newEntry
+  }
+}
